f0d7cba54f5b3825f52ca8bef3f3f4e2efcb728f
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-06-13 15:53:30 +0200

    KATA NOT FULLY TESTED

--- RomanNumeralsKata/RomanNumeralsKata.Tests/RomanNumeralsConverterTests.cs
+++ RomanNumeralsKata/RomanNumeralsKata.Tests/RomanNumeralsConverterTests.cs
@@ -10,17 +10,146 @@ namespace RomanNumeralsKata.Tests
     [TestFixture]
     public class RomanNumeralsConverterTests
     {
-        [Test]
-        public void Convert_GivenNumber1_ShouldReturnI()
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        public void Convert_GivenNumber1_2_Or_3_ShouldAppendIAccordingly(int number, string expected)
         {
             //Arrange
             var sut = new RomanNumeralsConverter();
-            var number = "1";
-            var expected = "I";
             //Act
             var actual = sut.Convert(number);
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        public void Convert_GivenNumber4_ShouldReturnIV()
+        {
+            //Arrange
+            var sut = new RomanNumeralsConverter();
+            var number = 4;
+            var expected = "IV";
+            //Act
+            var actual = sut.Convert(number);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Convert_GivenNumber5_ShouldReturnV()
+        {
+            //Arrange
+            var sut = new RomanNumeralsConverter();
+            var number = 5;
+            var expected = "V";
+            //Act
+            var actual = sut.Convert(number);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        public void Convert_GivenNumber6_7_Or_8_ShouldAppendIAccordingly(int number, string expected)
+        {
+            //Arrange
+            var sut = new RomanNumeralsConverter();
+            //Act
+            var actual = sut.Convert(number);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Convert_GivenNumber9_ShouldReturnIX()
+        {
+            //Arrange
+            var sut = new RomanNumeralsConverter();
+            var number = 9;
+            var expected = "IX";
+            //Act
+            var actual = sut.Convert(number);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Convert_GivenNumber10_ShouldReturnIX()
+        {
+            //Arrange
+            var sut = new RomanNumeralsConverter();
+            var number = 10;
+            var expected = "X";
+            //Act
+            var actual = sut.Convert(number);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(10, "X")]
+        [TestCase(14, "XIV")]
+        [TestCase(19, "XIX")]
+        public void Convert_GivenTeenNumber_ShouldAppendAccordingly(int number, string expected)
+        {
+            //Arrange
+            var sut = new RomanNumeralsConverter();
+            //Act
+            var actual = sut.Convert(number);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(20, "XX")]
+        [TestCase(26, "XXVI")]
+        [TestCase(29, "XXIX")]
+        public void Convert_GivenTwentysCompoundNumber_ShouldAppendAccordingly(int number, string expected)
+        {
+            //Arrange
+            var sut = new RomanNumeralsConverter();
+            //Act
+            var actual = sut.Convert(number);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(30, "XXX")]
+        [TestCase(34, "XXXIV")]
+        [TestCase(38, "XXXVIII")]
+        public void Convert_GivenThirtysCompoundNumber_ShouldAppendAccordingly(int number, string expected)
+        {
+            //Arrange
+            var sut = new RomanNumeralsConverter();
+            //Act
+            var actual = sut.Convert(number);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(40, "XL")]
+        [TestCase(43, "XLIII")]
+        [TestCase(47, "XLVII")]
+        public void Convert_GivenFortysCompoundNumber_ShouldAppendAccordingly(int number, string expected)
+        {
+            //Arrange
+            var sut = new RomanNumeralsConverter();
+            //Act
+            var actual = sut.Convert(number);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        //[Test]
+        //public void Convert_GivenNumber40_ShouldReturnXL()
+        //{
+        //    //Arrange
+        //    var sut = new RomanNumeralsConverter();
+        //    var number = 40;
+        //    var expected = "XL";
+        //    //Act
+        //    var actual = sut.Convert(number);
+        //    //Assert
+        //    Assert.AreEqual(expected, actual);
+        //}
     }
 }
--- RomanNumeralsKata/RomanNumeralsKata/RomanNumeralsConverter.cs
+++ RomanNumeralsKata/RomanNumeralsKata/RomanNumeralsConverter.cs
@@ -8,9 +8,37 @@ namespace RomanNumeralsKata
 {
     public class RomanNumeralsConverter
     {
-        public string Convert(string number)
+        public string Convert(int number)
         {
-            return "I";
+            int[] values = { 40, 20, 10, 9, 5, 4 };
+            string[] symbols = { "XL","XX", "X", "IX", "V", "IV" };
+
+            StringBuilder result = new StringBuilder();
+            int balance = number;
+           
+            for (int i = 0; i < values.Length; i++)
+            {
+                balance = AppendRomanNumerals(balance, values[i], symbols[i], result);
+            }
+            for (int i = 0; i < balance; i++)
+            {
+                result.Append("I");
+            }
+
+            return result.ToString();
         }
+
+        private int AppendRomanNumerals(int number, int values, string symbols, StringBuilder builder)
+        {
+            int result = number;
+            
+            if (result >= values)
+            {
+                builder.Append(symbols);
+                result -= values;
+            }
+            return result;
+        }
+
     }
 }
