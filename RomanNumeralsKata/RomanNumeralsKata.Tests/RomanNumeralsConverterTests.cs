e2e77bd64229830fb8d2f2957eb256ec8070c5a5
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-06-13 10:41:57 +0200

    1 test and setup

---
+++ RomanNumeralsKata/RomanNumeralsKata.Tests/RomanNumeralsConverterTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsKata.Tests
+{
+    [TestFixture]
+    public class RomanNumeralsConverterTests
+    {
+        [Test]
+        public void Convert_GivenNumber1_ShouldReturnI()
+        {
+            //Arrange
+            var sut = new RomanNumeralsConverter();
+            var number = "1";
+            var expected = "I";
+            //Act
+            var actual = sut.Convert(number);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
