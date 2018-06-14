08725d8ffe6f04fd9faa482025152cc4af5a21c4
Monique Govender <MoniqueG@SAHOMELOANS.COM>
2018-06-14 09:47:56 +0200

    some refactoring

--- RomanNumeralsKata/RomanNumeralsKata/RomanNumeralsConverter.cs
+++ RomanNumeralsKata/RomanNumeralsKata/RomanNumeralsConverter.cs
@@ -11,14 +11,14 @@ namespace RomanNumeralsKata
         public string Convert(int number)
         {
             int[] values = { 2000, 1000, 900, 500, 400, 200, 100, 90, 50, 40, 20, 10, 9, 5, 4 };
-            string[] symbols = { "MM", "M", "CM", "D", "CD", "CC", "C", "XC","L", "XL","XX", "X", "IX", "V", "IV" };
+            string[] numerals = { "MM", "M", "CM", "D", "CD", "CC", "C", "XC","L", "XL","XX", "X", "IX", "V", "IV" };
 
             StringBuilder result = new StringBuilder();
             int balance = number;
            
             for (int i = 0; i < values.Length; i++)
             {
-                balance = Join(balance, values[i], symbols[i], result);
+                balance = Join(balance, values[i], numerals[i], result);
             }
             for (int i = 0; i < balance; i++)
             {
@@ -28,13 +28,13 @@ namespace RomanNumeralsKata
             return result.ToString();
         }
 
-        private int Join(int number, int values, string symbols, StringBuilder builder)
+        private int Join(int number, int values, string numerals, StringBuilder builder)
         {
             int result = number;
             
             if (result >= values)
             {
-                builder.Append(symbols);
+                builder.Append(numerals);
                 result -= values;
             }
             return result;
